b383c608b7ce3a0f0ad3d810e8d6e3545a62a29f
MaxFancourtedX <gymsf@leeds.ac.uk>
2020-04-09 08:12:05 +0100

    Stage2 Commit, adding lots of the base pages so that i can transfer and continue to work on the main pc

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-app = Flask(__name__)
+from helpers import login_required
+ 
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,7 +23,15 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
+@login_required
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
---
+++ instance/config.py
@@ -0,0 +1,2 @@
+DEBUG = True
+SECRET_KEY = 'cj\x1b\xd9\xe1*:\xecc\xaf\xea,'
